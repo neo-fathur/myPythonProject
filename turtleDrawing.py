a60c7a749c6bfded31bc335d5b17af167557f347
neo-fathur <159503186+neo-fathur@users.noreply.github.com>
2025-01-18 20:13:03 +0700

    modified:   turtleDrawing.py

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -15,7 +15,6 @@ for i in 20, 30, 40:
         turtle.forward(100)
         turtle.left(90)
 
- 
 
 turtle.reset()
 turtle.speed(3)
@@ -46,19 +45,30 @@ turtle.shape("turtle")
 def draw_spiral(radius):
     original_xcor = turtle.xcor()
     original_ycor = turtle.ycor()
-    speed = 3
+    speed = 1
     while True:
         turtle.forward(speed)
         turtle.left(10)
         speed += 0.1
         if turtle.distance(original_xcor, original_ycor) > radius:
-            turtle.penup()
             break
 
-draw_spiral(150)
+draw_spiral(100)
+
+
+turtle.reset()
+
+turtle.left(90)
+
+color_list = ["black", "blue", "purple", "green"]
+for color in color_list:
+    turtle.forward(20)
+    turtle.color(color)
+    turtle.write("What color am I now?")
 
 
 default_value = "Python is awesome!"
+turtle.penup()
 turtle.goto(0, 250)
 turtle.write(input(f"Enter a value (default: {default_value}): ") or default_value, align="center", font=(None, 16, "bold"))
 turtle.goto(0, 210)
