6b25cad69f6ebfcd0b30fcbb7e185bd793b41ab0
neo-fathur <159503186+neo-fathur@users.noreply.github.com>
2025-01-20 18:34:19 +0700

    modified:   survey.py

--- survey.py
+++ survey.py
@@ -1,4 +1,6 @@
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 from pprint import pprint
 
 os.system('cls' if os.name == 'nt' else 'clear')    #clear the screen
@@ -56,3 +58,22 @@ for name in counts:
         winner_name = name
 print()
 print(f"The winner is: '{winner_name}' with {winner_votes} votes!")
+
+
+names = []
+votes = []
+# Split the dictionary of name:votes into two lists, one for names and one for vote count
+for radish in counts:
+    names.append(radish)
+    votes.append(counts[radish])
+
+# The X axis can just be numbered 0,1,2,3...
+x = np.arange(len(counts))
+
+plt.bar(x, votes)
+plt.xticks(ticks=x, labels=names, rotation=45, ha='right')
+plt.ylabel("Votes")
+plt.title('Radish Variety Votes')
+
+plt.tight_layout()
+plt.show()
