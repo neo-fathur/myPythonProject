8d6eed11f50641e6fbc522589817d1345d148106
neo-fathur <159503186+neo-fathur@users.noreply.github.com>
2025-01-21 18:27:21 +0700

    modified:   data_structures.py

--- data_structures.py
+++ data_structures.py
@@ -1,54 +1,57 @@
+from pprint import pprint
 import os
 os.system('cls' if os.name == 'nt' else 'clear')    #clear the screen
 
-#add a list
+#================================================================================================
 
-shopping_list = []
-
-shopping_list.append("milk")
-shopping_list.append("cheese")
-shopping_list.append("bread")
-shopping_list.remove("milk")
-
-if "eggs" not in shopping_list:
-    shopping_list.append("eggs")
-
-print(shopping_list)
-
-if "milk" in shopping_list:
-    print("Delicious!")
+#list
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+supplies.append('pencil') if 'pencil' not in supplies else None
+supplies.remove('flamethrowers') if 'flamethrowers' in supplies else None
+for index, item in enumerate(supplies): #enumerate() function returns two values: the index of the item in the list, and the item in the list itself.
+    print('Index ' + str(index) + ' in supplies is: ' + item)
 
 #================================================================================================
 
-#add a dictionary
+#dictionary
+picnicItems = {'apples': 5, 'cups': 2}
+print(f"I am bringing {picnicItems.get('cups', 0)} cups.") #if the key is in the dictionary, the get() method returns the value associated with that key.
+print(f"I am bringing {picnicItems.get('pies', 0)} pies.") #if the key is not in the dictionary, the get() method returns the default value, 0.
 
-foods = {}
 
-foods["banana"] = "A delicious and tasty treat!"
-foods["dirt"]   = "Not delicious. Not tasty. DO NOT EAT!"
-
-if "cheese" not in foods:
-    foods["cheese"] = "Cheese is one of the known foods!"
-
-del foods["dirt"]
-
-print(foods)
-
-#================================================================================================
-#add a list to a dictionary:
-
-ingredients = {}
-
-ingredients["blt sandwich"] = ["bread", "lettuce", "tomato", "bacon"]
-
-print(ingredients)
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message:
+    count.setdefault(character, 0) #if the key is not in the dictionary, the setdefault() method adds the key to the dictionary with the value 0.
+    count[character] = count[character] + 1
+pprint(count)    
 
 #================================================================================================
+
+#nested dictionaries
+allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
+             'Bob': {'ham sandwiches': 3, 'apples': 2},
+             'Carol': {'cups': 3, 'apple pies': 1}}
+
+def totalBrought(guests, item):
+    numBrought = 0
+    for k, v in guests.items(): #items() method returns a list of key-value pairs in a dictionary.
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
+
+print('Number of things being brought:')
+print(f" - Apples         {totalBrought(allGuests, 'apples')}")
+print(f" - Cups           {totalBrought(allGuests, 'cups')}")
+print(f" - Cakes          {totalBrought(allGuests, 'cakes')}")
+print(f" - Ham Sandwiches {totalBrought(allGuests, 'ham sandwiches')}")
+print(f" - Apple Pies     {totalBrought(allGuests, 'apple pies')}")
+
+#================================================================================================
+
 #add dictionaries to a list:
-
 europe = []
 
 europe.append({"name": "Germany", "population": 81000000})
 europe.append({"name": "Luxembourg", "population": 512000})
 
-print(europe)
+pprint(europe)
