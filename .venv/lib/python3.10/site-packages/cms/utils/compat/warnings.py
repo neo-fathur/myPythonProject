31e1643b989f837bb3590e8cf48f76dd3576977d
neo-fathur <159503186+neo-fathur@users.noreply.github.com>
2025-01-28 20:28:07 +0700

    modified:   .venv/bin/django-admin

--- .venv/lib/python3.10/site-packages/cms/utils/compat/warnings.py
+++
@@ -1,8 +0,0 @@
-class RemovedInDjangoCMS42Warning(DeprecationWarning):
-    pass
-
-class RemovedInDjangoCMS43Warning(DeprecationWarning):
-    pass
-
-class RemovedInDjangoCMS50Warning(DeprecationWarning):
-    pass
